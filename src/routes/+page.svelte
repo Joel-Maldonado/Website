64e7976ee96f273e3ee3020eb0fc2649656fb2f2
Joel-Maldonado <jmaldonadoruiz0@gmail.com>
2024-11-29 15:45:15 -0800

    Background component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,51 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	import { fade, fly } from 'svelte/transition';
+	import Icon from '@iconify/svelte';
+	import Background from '$lib/components/Background.svelte';
+
+	let animate = $state(false);
+	onMount(() => {
+		animate = true;
+	});
 </script>
 
-<svelte:head>
-	<title>Joel M.</title>
-	<meta
-		name="description"
-		content="The software development portfolio and blog website for Joel Maldonado-Ruiz"
-	/>
-</svelte:head>
+<header class="relative h-screen overflow-hidden bg-black/40 p-12 text-center">
+	<Background />
+	{#if animate}
+		<div class="relative z-10 flex h-full flex-col items-center justify-center gap-6">
+			<h1 class="font-poppins text-8xl text-white">Joel Maldonado-Ruiz</h1>
 
-<header
-	class="relative h-screen overflow-hidden bg-transparent p-12 text-center transition-colors dark:bg-black"
->
-	<div class="relative z-10 flex h-full flex-col items-center justify-center gap-6">
-		<h1 class="font-poppins text-6xl font-bold text-black dark:text-white" transition:fade>
-			Joel Maldonado-Ruiz
-		</h1>
-
-		<p class="text-lg text-gray-800 dark:text-gray-200">
-			Visit <a
-				href="https://svelte.dev/docs/kit"
-				class="text-blue-600 hover:underline dark:text-blue-400">svelte.dev/docs/kit</a
-			> to read the documentation
-		</p>
-	</div>
+			<ul class="flex items-center px-16">
+				<li>
+					<a href="https://github.com/Joel-Maldonado" target="_blank" rel="noopener"
+						><Icon icon="mdi:github" class="text-6xl text-white" /></a
+					>
+				</li>
+				<li class="mx-4 h-12 w-px bg-white/20"></li>
+				<li>
+					<a href="https://www.linkedin.com/in/joel-maldonado-ruiz/" target="_blank" rel="noopener"
+						><Icon icon="mdi:linkedin" class="text-6xl text-white" /></a
+					>
+				</li>
+				<li class="mx-4 h-12 w-px bg-white/20"></li>
+				<li>
+					<a href="mailto:maldonjo@oregonstate.edu" target="_blank" rel="noopener"
+						><Icon icon="mdi:email" class="text-6xl text-white" /></a
+					>
+				</li>
+				<li class="mx-4 h-12 w-px bg-white/20"></li>
+				<li>
+					<!-- TODO: Fix resume link -->
+					<a href="https://drive.google.com/file/d/1dlkfjsdlksd" target="_blank" rel="noopener"
+						><Icon icon="mdi:file-document" class="text-6xl text-white" /></a
+					>
+				</li>
+			</ul>
+		</div>
+	{/if}
 </header>
 
 <style>
-	:global(.dark) {
-		color-scheme: dark;
-	}
+	/* Remove dark mode styles */
 </style>
